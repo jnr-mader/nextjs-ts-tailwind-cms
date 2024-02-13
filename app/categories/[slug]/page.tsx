710eda50de0c1ebfc25e015a4d07cf524ea391ae
juniorm <junior.mader@pinnacle.com>
2024-02-13 17:13:06 +0000

    cms test content added

---
+++ app/categories/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { getDocuments } from 'outstatic/server'
+
+export default async function Index() {
+  const posts = await getData()
+  return posts.map((post) => <h1 key={post.slug}>{post.title}</h1>)
+}
+
+async function getData() {
+  const posts = getDocuments('TechStack', ['title'])
+  return posts
+}
