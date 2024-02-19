414e52026d2cffed2810b9a187668a1d58d54b98
juniorm <junior.mader@pinnacle.com>
2024-02-19 23:31:32 +0000

    Toggle switch added and styled

---
+++ app/components/ToggleSwitch.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, { useState } from 'react'
+import Image from 'next/image';
+import lightIcon from "@/public/images/light-icon.svg"; 
+import darkIcon from "@/public/images/dark-icon.svg"; 
+
+export default function ToggleSwitch() {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked)
+  }
+
+  return (
+    <div className='flex sticky justify-end w-full top-[calc(100vh-3rem)]'>
+      <label className='relative inline-flex py-1 px-1 cursor-pointer select-none items-center justify-center rounded-md bg-orange-600'>
+        <input
+          type='checkbox'
+          className='sr-only'
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <span
+          className={`flex items-center space-x-[6px] rounded py-1 px-1 text-sm font-medium gap-1 ${
+            !isChecked ? 'text-primary bg-[--background-color]' : 'bg-orange-600 text-[--tabs-color]'
+          }`}
+        >
+        <Image 
+            src={lightIcon}
+            alt="Jnr"
+            priority
+        />          
+          light
+        </span>
+        <span
+          className={`flex items-center space-x-[6px] rounded py-1 px-1 text-sm font-medium gap-1 ${
+            isChecked ? 'text-primary bg-[--background-color]' : 'bg-orange-600 text-[--tabs-color]'
+          }`}
+        >
+        <Image 
+            src={darkIcon}
+            alt="Jnr"
+            priority
+        />              
+          dark
+        </span>
+      </label>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import NavLogo from "./components/NavLogo";
+import ToggleSwitch from "./components/ToggleSwitch";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center h-[5000px]">
+    <main className="flex mx-auto max-w-[1280px] min-h-screen flex-col items-center h-[5000px]">
+      <ToggleSwitch />
       <NavLogo />
-      <div>        
+      <div className='py-10 text-4xl text-center text-balance'>        
         <p>With over ten years of commercial experience, building and maintaining pixel perfect responsive websites.</p>
         <p>Specialising in front end development, with a passion for creating beautiful and functional user interfaces.</p>
         <p>Have a look around and please feel free to say hi.</p>
