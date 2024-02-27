c444b0bc2344cb4591e0b638f21c23b764782359
juniorm <junior.mader@pinnacle.com>
2024-02-27 17:00:58 +0000

    apiSErvice updated

--- app/apiService/index.ts
+++ app/apiService/index.ts
@@ -1,6 +1,6 @@
 import { getDocuments } from 'outstatic/server'
 
 export async function getData(targetCategory: string) {
-  const data = getDocuments(targetCategory, ['title', 'publishedAt', 'slug'])
+  const data = getDocuments(targetCategory, ['title', 'publishedAt', 'status', 'description', 'coverImage'])
   return data
 }
--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,6 +1,17 @@
 import { getData } from '@/app/apiService'
+import Image from 'next/image';
 
 export default async function Index() {
   const categories = await getData('TechStack')
-  return categories.map((category) => <h1 key={category.publishedAt}>{category.title}</h1>)
+
+  return (
+    <div className="grid gap-4 grid-cols-6">
+      {categories.map((category) =>(
+      <div key={category.publishedAt} className="flex bg-orange-600 text-white rounded-md border-black border-2">
+        <p>{category.title}</p>
+        <Image src={category.coverImage as string} width='150' height={'150'} alt={category.title}/>
+      </div>
+      ))}
+    </div>
+  )
 }
